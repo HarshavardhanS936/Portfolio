4547792bfe58545bc51fb045bd9352179c18241b
HarshavardhanS936 <shv9306@gmail.com>
2026-02-21 22:08:02 +0530

    update

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,5 @@
 export const projects = [
-    
+
     {
         id: 1,
         title: "RF-Based Road Sign Detection and Driver Alert",
@@ -7,8 +7,7 @@ export const projects = [
         description: "",
         techStack: ["Nodemcu", "Rf 433mhz transmitter", "Rf 433mhz receiver", "12v Adapter", "Power Supply", "Buzzer", "Lcd Display", "9v Battery", "Connecting Wires"],
         image: "https://placehold.co/600x400/1a1a1a/764ba2?text=Road+Sign+Detection",
-        github: "#",
-        live: "#"
+        github: "https://github.com/HarshavardhanS936/RF-based-road-sign-detection"
     },
     {
         id: 2,
@@ -17,8 +16,8 @@ export const projects = [
         description: "Intelligent chatbot integrated with Large Language Models for natural conversations.",
         techStack: ["Python", "OpenAI API", "React"],
         image: "https://placehold.co/600x400/1a1a1a/f5576c?text=AI+Chatbot",
-        github: "#",
-        live: "#"
+        github: "https://github.com/HarshavardhanS936/Ai_Agent",
+        live: "https://ai-agent-sepia.vercel.app/"
     },
     {
         id: 3,
@@ -27,10 +26,21 @@ export const projects = [
         description: "Personal portfolio website with modern design, animations, and dark mode.",
         techStack: ["React", "Tailwind CSS", "Framer Motion"],
         image: "https://placehold.co/600x400/1a1a1a/667eea?text=Portfolio",
-        github: "#",
-        live: "#"
-    },{
+        github: "https://github.com/HarshavardhanS936/Portfolio",
+        live: "https://harshavardhan001.netlify.app/"
+    },
+    {
         id: 4,
+        title: "BINGO-Game",
+        category: "Web",
+        description: "BINGO-Game with modern design, animations, and dark mode.",
+        techStack: ["HTML", "CSS", "JavaScript"],
+        image: "https://placehold.co/600x400/1a1a1a/667eea?text=BINGO+Game",
+        github: "https://github.com/HarshavardhanS936/BINGO_Game",
+        live: "https://bingo-client-1z4i.onrender.com"
+    },
+    {
+        id: 5,
         title: "V8 Engine with Alcohol Detector",
         category: "Prototype",
         description: "Alcohol detector - To avoid drunk and drive accidents",
